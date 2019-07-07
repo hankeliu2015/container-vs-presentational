2a683bc34e5484cf894709ec2d4d4a3dba9a44c8
NickisKnowledge <lifechangingstuff@gmail.com>
2019-07-07 15:06:02 -0500

    Create a form and handlers to handle changes

--- src/containers/todonew.js
+++ src/containers/todonew.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
 
 class TodoNew extends Component {
+  state = {
+    content: '',
+    completed: false
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target;
+
+    this.setState({ [name]: value })
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    
+  }
+
   render() {
-    return(<div>test</div>)
+    return(
+      <div>
+        <h1>Add A ToDo!</h1>
+        <form onSubmit={this.submitForm}>
+          <label>Content: </label><input type='text' name='content' value={this.state.content} onChange={this.handleChange}/><br />
+          <label>Task completed?</label><input type='checkbox' name='completed' value={this.state.completed}onChange={this.handleChange}/><br />
+          <input type='submit' value='Add ToDo'/>
+        </form>
+      </div>
+    )
   }
 }
 

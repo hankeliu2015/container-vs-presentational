8ac468b8a051379c3db3825d19f221b9670d269c
NickisKnowledge <lifechangingstuff@gmail.com>
2019-07-07 14:22:31 -0500

    create container components

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Nav from './components/nav'
-// import Nav from './components/nav'
+import TodoList from './containers/todolist'
+import TodoNew from './containers/todonew'
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+
   render() {
     return (
       <div>
-       <Nav />
+        <Router>
+          <Nav />
+          <div className='App'>
+            <Switch>
+              <Route exact path='/' render={(props) => <TodoList {...props} todos={this.state.todos} />} />
+              <Route exact path='/new' render={(props) => <TodoNew {...props} createTodo={this.createTodo} />} />
+            </Switch>
+          </div>
+        </Router>
       
       </div>
      );
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
+import { domainToASCII } from 'url';
 class Nav extends Component {
   render() {
-    return(<h1>HELLLLOOO</h1>)
+    return(
+      <nav>
+        <ul>
+          <li><Link to='/'>ToDo List</Link></li>
+          <li><Link to='/new'>Create ToDo</Link></li>
+        </ul>
+      </nav>
+    )
   }
 }
 
